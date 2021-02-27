5e20246bd6ab9909b5a1f3ec42f03c121b4e7ea4
Camden Mecklem <c@camdenmecklem.com>
2021-02-26 20:53:49 -0500

    update navbar with hamburger menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import icon from '../images/icon.png'
 
 const Navbar = () => {
+  const [menuVisible, setMenuVisible] = useState(false)
+
+  const checkIfActive = (base: string) => {
+    return menuVisible
+      ? `${base} is-active`
+      : base
+  }
+
   return (
     <div className='navbar is-family-secondary'>
       <div className='container'>
@@ -10,19 +18,24 @@ const Navbar = () => {
           <Link className='navbar-item' to='/'>
             <img src={icon} alt='home' />
           </Link>
+          <div role='button' className={checkIfActive('navbar-burger')} onClick={() => setMenuVisible(!menuVisible)} aria-label='menu' aria-expanded='false' data-target='navMenu'>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </div>
         </div>
-        <div className='navbar-menu'>
+        <div className={checkIfActive('navbar-menu')}>
           <div className='navbar-end'>
-            <Link className='navbar-item' to='/alphabet'>
+            <Link className='navbar-item' to='/alphabet' onClick={() => setMenuVisible(false)}>
               Alphabet
             </Link>
-            <Link className='navbar-item' to='/texts'>
+            <Link className='navbar-item' to='/texts' onClick={() => setMenuVisible(false)}>
               Texts
             </Link>
-            <Link className='navbar-item' to='/history'>
+            <Link className='navbar-item' to='/history' onClick={() => setMenuVisible(false)}>
               History
             </Link>
-            <Link className='navbar-item' to='/resources'>
+            <Link className='navbar-item' to='/resources' onClick={() => setMenuVisible(false)}>
               Resources
             </Link>
           </div>
